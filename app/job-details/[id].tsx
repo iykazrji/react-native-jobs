8d7dc98f0ac02fedff65fdc2e1437bc1867dd94d
Iyk Azorji <iykazorji@gmail.com>
2023-05-22 21:53:56 -0400

    Updated to TSX

---
+++ app/job-details/[id].tsx
@@ -0,0 +1,16 @@
+import {
+	View,
+	Text,
+	SafeAreaView,
+	ScrollView,
+	ActivityIndicator,
+	RefreshControl,
+} from "react-native";
+
+export default function JobDetails() {
+	return (
+		<View>
+			<Text>Job Details</Text>
+		</View>
+	);
+}
